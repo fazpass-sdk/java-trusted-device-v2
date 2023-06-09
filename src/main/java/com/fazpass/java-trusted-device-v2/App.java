c413de72944bedd54fd4d6a927eda0189dee533d
Andri nova riswanto <anvarisy@gmail.com>
2023-06-09 11:19:04 +0700

    Initialize Project

---
+++ src/main/java/com/fazpass/java-trusted-device-v2/App.java
@@ -0,0 +1,13 @@
+package com.fazpass.java-trusted-device-v2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
