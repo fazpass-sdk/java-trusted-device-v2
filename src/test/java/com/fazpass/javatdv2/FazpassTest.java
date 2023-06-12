f406238aa7d04ed0c790e8eff78a5a7d89c30e95
Andri nova riswanto <anvarisy@gmail.com>
2023-06-12 17:10:39 +0700

    Update pom file

--- src/test/java/com/fazpass/javatdv2/FazpassTest.java
+++ src/test/java/com/fazpass/javatdv2/FazpassTest.java
@@ -1,6 +1,34 @@
 package com.fazpass.javatdv2;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import junit.framework.TestCase;
 
 public class FazpassTest extends TestCase {
+    @Test
+    void initializeShouldThrowWhenPrivateKeyIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> Fazpass.initialize(null, "http://localhost"));
+    }
+
+    @Test
+    void initializeShouldThrowWhenPrivateKeyIsEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> Fazpass.initialize("", "http://localhost"));
+    }
+
+    @Test
+    void initializeShouldThrowWhenBaseUrlIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> Fazpass.initialize("privateKey", null));
+    }
+
+    @Test
+    void initializeShouldThrowWhenBaseUrlIsEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> Fazpass.initialize("privateKey", ""));
+    }
+
+    @Test
+    void initializeShouldReturnTrustedDeviceWhenParametersAreValid() {
+        TrustedDevice trustedDevice = Fazpass.initialize("privateKey", "http://localhost");
+        assertNotNull(trustedDevice);
+        assertTrue(trustedDevice instanceof TrustedDeviceImpl);
+    }
 }
--- src/test/java/com/fazpass/javatdv2/TrustedDeviceImplTest.java
+++ src/test/java/com/fazpass/javatdv2/TrustedDeviceImplTest.java
@@ -53,4 +53,132 @@ public class TrustedDeviceImplTest extends TestCase {
         assertTrue(result.isPresent());
         assertEquals("fazpass_id", result.get().getFazpassId());
     }
+
+    @Test
+    public void testEnrollDevice() throws FazpassException {
+        // membuat mock
+        TrustedDevice trustedDevice = Mockito.mock(TrustedDeviceImpl.class);
+
+        // membuat dummy Device
+        Device device = new Device();
+        device.setFazpassId("fazpass_id");
+
+        // menentukan perilaku mock
+        when(trustedDevice.enrollDevice("pic_id", "meta_data")).thenReturn(device);
+
+        // memanggil metode yang diuji
+        Device result = trustedDevice.enrollDevice("pic_id", "meta_data");
+
+        // verifikasi
+        assertNotNull(result);
+        assertEquals("fazpass_id", result.getFazpassId());
+    }
+
+    @Test
+    public void testEnrollAsyncDevice() throws ExecutionException, InterruptedException {
+        // membuat mock
+        TrustedDevice trustedDevice = Mockito.mock(TrustedDeviceImpl.class);
+
+        // membuat dummy Device
+        Device device = new Device();
+        device.setFazpassId("fazpass_id");
+
+        // menentukan perilaku mock
+        when(trustedDevice.enrollAsyncDevice("pic_id", "meta_data"))
+                .thenReturn(CompletableFuture.completedFuture(Optional.of(device)));
+
+        // memanggil metode yang diuji
+        Optional<Device> result = trustedDevice.enrollAsyncDevice("pic_id", "meta_data").get();
+
+        // verifikasi
+        assertNotNull(result);
+        assertTrue(result.isPresent());
+        assertEquals("fazpass_id", result.get().getFazpassId());
+    }
+
+    @Test
+    public void testValidateDevice() throws FazpassException {
+        // membuat mock
+        TrustedDevice trustedDevice = Mockito.mock(TrustedDeviceImpl.class);
+
+        // membuat dummy Device
+        Device device = new Device();
+        device.setFazpassId("fazpass_id");
+        device.setScoring(90.0);
+
+        // menentukan perilaku mock
+        when(trustedDevice.validateDevice("fazpass_id", "meta_data")).thenReturn(device);
+
+        // memanggil metode yang diuji
+        Device result = trustedDevice.validateDevice("fazpass_id", "meta_data");
+
+        // verifikasi
+        assertNotNull(result);
+        assertEquals(90.0, result.getScoring());
+    }
+
+    @Test
+    public void testValidateAsyncDevice() throws ExecutionException, InterruptedException {
+        // membuat mock
+        TrustedDevice trustedDevice = Mockito.mock(TrustedDeviceImpl.class);
+
+        // membuat dummy Device
+        Device device = new Device();
+        device.setFazpassId("fazpass_id");
+        device.setScoring(90.0);
+        // menentukan perilaku mock
+        when(trustedDevice.validateAsyncDevice("fazpass_id", "meta_data"))
+                .thenReturn(CompletableFuture.completedFuture(Optional.of(device)));
+
+        // memanggil metode yang diuji
+        Optional<Device> result = trustedDevice.validateAsyncDevice("fazpass_id", "meta_data").get();
+
+        // verifikasi
+        assertNotNull(result);
+        assertTrue(result.isPresent());
+        assertEquals(90.0, result.get().getScoring());
+    }
+
+    @Test
+    public void testRemoveDevice() throws FazpassException {
+        // membuat mock
+        TrustedDevice trustedDevice = Mockito.mock(TrustedDeviceImpl.class);
+
+        // membuat dummy Device
+        Device device = new Device();
+        device.setFazpassId("fazpass_id");
+
+        // menentukan perilaku mock
+        when(trustedDevice.removeDevice("pic_id", "meta_data")).thenReturn(device);
+
+        // memanggil metode yang diuji
+        Device result = trustedDevice.removeDevice("pic_id", "meta_data");
+
+        // verifikasi
+        assertNotNull(result);
+        assertEquals("fazpass_id", result.getFazpassId());
+    }
+
+    @Test
+    public void testRemoveAsyncDevice() throws ExecutionException, InterruptedException {
+        // membuat mock
+        TrustedDevice trustedDevice = Mockito.mock(TrustedDeviceImpl.class);
+
+        // membuat dummy Device
+        Device device = new Device();
+        device.setFazpassId("fazpass_id");
+
+        // menentukan perilaku mock
+        when(trustedDevice.removeAsyncDevice("pic_id", "meta_data"))
+                .thenReturn(CompletableFuture.completedFuture(Optional.of(device)));
+
+        // memanggil metode yang diuji
+        Optional<Device> result = trustedDevice.removeAsyncDevice("pic_id", "meta_data").get();
+
+        // verifikasi
+        assertNotNull(result);
+        assertTrue(result.isPresent());
+        assertEquals("fazpass_id", result.get().getFazpassId());
+    }
+
 }
